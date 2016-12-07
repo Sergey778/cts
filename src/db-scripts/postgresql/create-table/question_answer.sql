586ec68f2226024e3e9ebf7169fd12b176fc3dac
Sergey <sergeygerasimov778@gmail.com>
2016-12-07 13:50:53 +0300

    Update question answers

--- src/db-scripts/postgresql/create-table/question_answer.sql
+++ src/db-scripts/postgresql/create-table/question_answer.sql
@@ -2,12 +2,14 @@
 DROP TABLE IF EXISTS question_answer;
 
 CREATE TABLE question_answer (
+  question_answer_id BIGINT NOT NULL,
   question_id BIGINT NOT NULL,
-  question_answer_value VARCHAR(1024) NOT NULL,
+  question_answer_value TEXT NOT NULL,
   question_answer_creator_id BIGINT NOT NULL,
-  CONSTRAINT question_answer_pk PRIMARY KEY (question_id, question_answer_value),
+  question_answer_tomita_xml TEXT NULL DEFAULT NULL,
+  CONSTRAINT question_answer_pk PRIMARY KEY (question_id, question_answer_id),
   CONSTRAINT question_answer_question_fk FOREIGN KEY (question_id)
     REFERENCES question (question_id) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT question_answer_creator_fk FOREIGN KEY (question_answer_creator_id)
-    REFERENCES "user" (user_id) ON DELETE CASCADE ON UPDATE CASCADE 
-)
+    REFERENCES "user" (user_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
--- src/main/scala/db/QuestionAnswer.scala
+++ src/main/scala/db/QuestionAnswer.scala
@@ -2,30 +2,49 @@ package db
 
 import scalikejdbc._
 
-case class QuestionAnswer(question: Question, answer: String, creator: User)
+case class QuestionAnswer(id: BigInt, question: Question, answer: String, creator: User, xml: Option[String] = None)
 
 object QuestionAnswer {
-  def create(question: Question, user: User, answer: String) = {
+
+  private var cache = Map[Question, BigInt]()
+
+  private def getIdFromDb(question: Question) = DB readOnly { implicit session =>
+    sql"SELECT COUNT(*) FROM question_answer WHERE question_id = ${question.id}"
+      .map(rs => BigInt(rs.bigInt(1)))
+      .single()
+      .apply()
+  }
+
+  private def getId(question: Question) = {
+    val value = cache.getOrElse(question, getIdFromDb(question).getOrElse(BigInt(0))) + 1
+    cache = cache.updated(question, value)
+    value
+  }
+
+  def create(question: Question, user: User, answer: String): Option[QuestionAnswer] = {
+    val id = getId(question)
     val result = using(DB(ConnectionPool.borrow())) { db =>
       db localTx { implicit session =>
         sql"""
-             INSERT INTO question_answer(question_id, question_answer_value, question_answer_creator_id)
-              VALUES (${question.id}, $answer, ${user.id})
+             INSERT INTO question_answer(question_answer_id, question_id, question_answer_value, question_answer_creator_id)
+              VALUES ($id, ${question.id}, $answer, ${user.id})
            """
           .update()
           .apply()
       }
     }
-    if (result > 0) Some(QuestionAnswer(question, answer, user)) else None
+    if (result > 0) Some(QuestionAnswer(id, question, answer, user)) else None
   }
 
   def fromQuestion(question: Question) = using(DB(ConnectionPool.borrow())) { db =>
     db readOnly { implicit session =>
       sql"""
            SELECT
+            question_answer_id,
             question_id,
             question_answer_value,
-            question_answer_creator_id
+            question_answer_creator_id,
+            question_answer_tomita_xml
            FROM
             question_answer
            WHERE question_id = ${question.id}
@@ -37,14 +56,18 @@ object QuestionAnswer {
   }
 
   def fromResultSet(x: WrappedResultSet, question: Question) = QuestionAnswer(
+    id = x.bigInt("question_answer_id"),
     question = question,
     answer = x.string("question_answer_value"),
-    creator = User.findById(x.bigInt("question_answer_creator_id")).get
+    creator = User.findById(x.bigInt("question_answer_creator_id")).get,
+    xml = x.stringOpt("question_answer_tomita_xml")
   )
 
   def fromResultSet(x: WrappedResultSet) = QuestionAnswer(
+    id = x.bigInt("question_answer_id"),
     question = Question.findById(x.bigInt("question_id")).get,
     answer = x.string("question_answer_value"),
-    creator = User.findById(x.bigInt("question_answer_creator_id")).get
+    creator = User.findById(x.bigInt("question_answer_creator_id")).get,
+    xml = x.stringOpt("question_answer_tomita_xml")
   )
 }
